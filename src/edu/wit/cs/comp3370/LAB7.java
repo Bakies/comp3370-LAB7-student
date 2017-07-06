36c12de34d5bb122b0823c5368387468527e97e4
Jon Bakies <jon@baki.es>
2017-07-05 20:27:15 -0400

    I did it

--- src/edu/wit/cs/comp3370/LAB7.java
+++ src/edu/wit/cs/comp3370/LAB7.java
@@ -1,10 +1,5 @@
 package edu.wit.cs.comp3370;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
-
 /* Finds the longest common substring in two text files 
  * 
  * Wentworth Institute of Technology
@@ -14,60 +9,128 @@ import java.util.Scanner;
  */
 
 public class LAB7 {
-	
+
 	//TODO Document this method
 	public static String[] findLCSdyn(String text1, String text2) {
-		// TODO Implement this method
-		
-		// TODO set static var "longest" to longest common subsequence length
-		return new String[]{"", ""};
+		int[][] arrows = calcLCS(text1, text2);
+
+		StringBuilder w1 = new StringBuilder(), w2 = new StringBuilder();
+		int x = text1.length() - 1, y = text2.length() - 1;
+
+		test: while (true) {
+			if (x < 0 || y < 0) {
+				// prepend rest of words
+				System.out.println("Gonna break");
+				for (; x >= 0; x--) {
+					w1.insert(0, text1.charAt(x));
+					w2.insert(0, '-');
+				}
+				for (; y >= 0; y--) {
+					w2.insert(0, text2.charAt(y));
+					w1.insert(0, '-');
+				}
+
+				break test;
+			}
+			if (arrows[x + 1][y + 1] == 3) {
+				w1.insert(0, text1.charAt(x));
+				x--;
+				w2.insert(0, text2.charAt(y));
+				y--;
+			} else if (arrows[x + 1][y + 1] == 1) {
+				w1.insert(0, text1.charAt(x));
+				x--;
+				w2.insert(0, '-');
+			} else {
+				w2.insert(0, text2.charAt(y));
+				y--;
+				w1.insert(0, '-');
+			}
+		}
+
+		return new String[] { w1.toString(), w2.toString() };
+
 	}
-	
+
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return int[][] b which is the LCS arrows in the book, up = 1, left = 2,
+	 *         upleft = 1|2
+	 */
+	public static int[][] calcLCS(String x, String y) {
+		int m = x.length(), n = y.length();
+
+		int[][] b = new int[m + 1][n + 1];
+		int[][] c = new int[m + 1][n + 1];
+		for (int i = 0; i < m; i++)
+			c[i][0] = 0;
+		for (int j = 0; j < n; j++)
+			c[0][j] = 0;
+
+		for (int i = 1; i <= m; i++)
+			for (int j = 1; j <= n; j++) {
+				if (x.charAt(i - 1) == y.charAt(j - 1)) {
+					c[i][j] = c[i - 1][j - 1] + 1;
+					b[i][j] = 3;
+				} else if (c[i - 1][j] >= c[i][j - 1]) {
+					c[i][j] = c[i - 1][j];
+					b[i][j] = 1;
+				} else {
+					c[i][j] = c[i][j - 1];
+					b[i][j] = 2;
+				}
+			}
+
+		longest = c[m][n];
+		return b;
+	}
+
 	/********************************************
 	 * 
 	 * You shouldn't modify anything past here
 	 * 
 	 ********************************************/
-	
+
 	private static int longest = 0;
-	
+
 	// recursive helper for DFS
 	private static void dfs_solve(int i1, int i2, String s1, String s2, char[] out1, char[] out2, int score, int index) {
-	  
-	  if ((i1 >= s1.length()) && (i2 >= s2.length())) {
-	    if (score > longest) {
-	      out1[index] = '\0';
-	      out2[index] = '\0';
-	      longest = score;
-	      sol1 = String.valueOf(out1).substring(0, String.valueOf(out1).indexOf('\0'));
-	      sol2 = String.valueOf(out2).substring(0, String.valueOf(out2).indexOf('\0'));
-	    }
-	  }
-	  else if ((i1 >= s1.length()) && (i2 < s2.length())) {	// at the end of first string
-	    out1[index] = '-';
-	    out2[index] = s2.charAt(i2);
-	    dfs_solve(i1, i2 + 1, s1, s2, out1, out2, score, index+1);
-	  }
-	  else if ((i1 < s1.length()) && (i2 >= s2.length())) {	// at the end of second string
-	    out1[index] = s1.charAt(i1);
-	    out2[index] = '-';
-	    dfs_solve(i1 + 1, i2, s1, s2, out1, out2, score, index+1);
-	  }
-	  else {
-	    if (s1.charAt(i1) == s2.charAt(i2)) {	// matching next character
-	      out1[index] = s1.charAt(i1);
-	      out2[index] = s2.charAt(i2);
-	      dfs_solve(i1 + 1, i2 + 1, s1, s2, out1, out2, score + 1, index + 1);
-	    }
-	    
-	    out1[index] = '-';
-	    out2[index] = s2.charAt(i2);
-	    dfs_solve(i1, i2 + 1, s1, s2, out1, out2, score, index + 1);
-	    
-	    out1[index] = s1.charAt(i1);
-	    out2[index] = '-';
-	    dfs_solve(i1 + 1, i2, s1, s2, out1, out2, score, index + 1);
-	  }
+
+		if ((i1 >= s1.length()) && (i2 >= s2.length())) {
+			if (score > longest) {
+				out1[index] = '\0';
+				out2[index] = '\0';
+				longest = score;
+				sol1 = String.valueOf(out1).substring(0, String.valueOf(out1).indexOf('\0'));
+				sol2 = String.valueOf(out2).substring(0, String.valueOf(out2).indexOf('\0'));
+			}
+		} else if ((i1 >= s1.length()) && (i2 < s2.length())) { // at the end of
+			// first string
+			out1[index] = '-';
+			out2[index] = s2.charAt(i2);
+			dfs_solve(i1, i2 + 1, s1, s2, out1, out2, score, index + 1);
+		} else if ((i1 < s1.length()) && (i2 >= s2.length())) { // at the end of
+			// second string
+			out1[index] = s1.charAt(i1);
+			out2[index] = '-';
+			dfs_solve(i1 + 1, i2, s1, s2, out1, out2, score, index + 1);
+		} else {
+			if (s1.charAt(i1) == s2.charAt(i2)) { // matching next character
+				out1[index] = s1.charAt(i1);
+				out2[index] = s2.charAt(i2);
+				dfs_solve(i1 + 1, i2 + 1, s1, s2, out1, out2, score + 1, index + 1);
+			}
+
+			out1[index] = '-';
+			out2[index] = s2.charAt(i2);
+			dfs_solve(i1, i2 + 1, s1, s2, out1, out2, score, index + 1);
+
+			out1[index] = s1.charAt(i1);
+			out2[index] = '-';
+			dfs_solve(i1 + 1, i2, s1, s2, out1, out2, score, index + 1);
+		}
 
 	}
 
@@ -76,23 +139,27 @@ public class LAB7 {
 
 	// recursively searches for longest substring, checking all possible alignments
 	public static String[] findLCSdfs(String text1, String text2) {
-	  int max_len = text1.length() + text2.length() + 1;
-	  char[] out1 = new char[max_len];
-	  char[] out2 = new char[max_len];
-	  
-	  dfs_solve(0, 0, text1, text2, out1, out2, 0, 0);
-	  
-	  String[] ret = new String[2];
-	  ret[0] = sol1; ret[1] = sol2;
-	  return ret; 
+		int max_len = text1.length() + text2.length() + 1;
+		char[] out1 = new char[max_len];
+		char[] out2 = new char[max_len];
+
+		dfs_solve(0, 0, text1, text2, out1, out2, 0, 0);
+
+		String[] ret = new String[2];
+		ret[0] = sol1;
+		ret[1] = sol2;
+		return ret;
 	}	
-	
+
 	// returns the length of the longest string
 	public static int getLongest() {
 		return longest;
 	}
-	
+
 	public static void main(String[] args) {
+		findLCSdyn("bent", "note");
+		findLCSdyn("note", "bent");
+		/*
 		Scanner s = new Scanner(System.in);
 		String file1, file2, text1 = "", text2 = "";
 		System.out.printf("Enter <text1> <text2> <algorithm>, ([dfs] - depth first search, [dyn] - dynamic programming): ");
@@ -127,5 +194,6 @@ public class LAB7 {
 		s.close();
 		
 		System.out.printf("Best cost: %d\nLongest string alignment:\n%s\n\n%s\n", longest, result[0], result[1]);	
+		 */
 	}
 }
